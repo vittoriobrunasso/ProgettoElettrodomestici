fa4e2c4862459d83e5eb9974d471a6e062bd18d5
vittoriobrunasso <150332195+vittoriobrunasso@users.noreply.github.com>
2025-03-06 16:20:10 +0100

    Delete Brunasso_vittorio_elettrodomestici directory

--- Brunasso_vittorio_elettrodomestici/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.elettrodomestici.brunasso_vittorio_elettrodomestici {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.elettrodomestici.brunasso_vittorio_elettrodomestici to javafx.fxml;
-    exports org.elettrodomestici.brunasso_vittorio_elettrodomestici;
-}
